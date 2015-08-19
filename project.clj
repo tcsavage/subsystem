64d945d11246b3a75215ba640b5c10cc2bccdb5c
Tom Savage <tcsavage@gmail.com>
2015-08-19 10:07:17 +0100

    Expand description and add codox source linking config

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject subsystem "0.1.0-SNAPSHOT"
-  :description "Component subsystems."
+  :description "Component subsystems. Turn a system map into a component in a larger system."
   :url "http://github.com/tcsavage/subsystem"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -8,5 +8,7 @@
                  [com.stuartsierra/component "0.2.3"]]
   :plugins [[codox "0.8.12"]
             [jonase/eastwood "0.2.1"]]
-  :codox {:defaults {:doc/format :markdown}}
+  :codox {:defaults {:doc/format :markdown}
+          :src-dir-uri "http://github.com/tcsavage/subsystem/blob/master/"
+          :src-linenum-anchor-prefix "L"}
   :eastwood {:linters [:all]})
